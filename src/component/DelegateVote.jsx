a3a6d6b414adb483695ef6ff68b424b78b1495e6
pheranmie <okekunletobi@gmail.com>
2024-03-09 13:33:48 +0100

    first commit

--- src/component/DelegateVote.jsx
+++ src/component/DelegateVote.jsx
@@ -1,25 +1,39 @@
 import { Box, Button, Card, Flex, Text, TextField } from "@radix-ui/themes";
+import { useState } from "react";
+import useDelegateVote from "../hooks/useDelegateVote";
 
-const DelegateVote = ({ to, handleDelegate }) => {
-    return (
-        <Card size="2" style={{ width: 425 }}>
-            <Flex gap="" align="center">
-                <Box width={"100%"}>
-                    <Flex direction="column" gap="3">
-                        <label>
-                            <Text as="div" size="2" mb="1" weight="bold">
-                                Delegate&apos;s Address
-                            </Text>
-                            <TextField.Input placeholder="Enter Delegate's Address" />
-                        </label>
-                        <Button onClick={() => handleDelegate(to)}>
-                            Delegate vote
-                        </Button>
-                    </Flex>
-                </Box>
-            </Flex>
-        </Card>
-    );
+const DelegateVote = () => {
+  const [address, setAddress] = useState("");
+
+  const handleDelegate = useDelegateVote(address);
+  return (
+    <Card size="2" style={{ width: 425 }}>
+      <Flex gap="" align="center">
+        <Box width={"100%"}>
+          <Flex direction="column" gap="3">
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Delegate&apos;s Address
+              </Text>
+              <TextField.Input
+                placeholder="Enter Delegate's Address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </label>
+            <Button
+              onClick={async () => {
+                await handleDelegate();
+                setAddress("");
+              }}
+            >
+              Delegate vote
+            </Button>
+          </Flex>
+        </Box>
+      </Flex>
+    </Card>
+  );
 };
 
 export default DelegateVote;
---
+++ src/hooks/useDelegateVote.js
@@ -0,0 +1,37 @@
+import { useCallback } from "react";
+import { isSupportedChain } from "../utils";
+import { isAddress } from "ethers";
+import { getProvider } from "../constants/providers";
+import { getProposalsContract } from "../constants/contracts";
+import {
+  useWeb3ModalAccount,
+  useWeb3ModalProvider,
+} from "@web3modal/ethers/react";
+
+const useDelegateVote = (address) => {
+  const { chainId } = useWeb3ModalAccount();
+  const { walletProvider } = useWeb3ModalProvider();
+
+  return useCallback(async () => {
+    if (!isSupportedChain(chainId)) return console.error("Wrong network");
+    if (!isAddress(address)) return console.error("Invalid address");
+    const readWriteProvider = getProvider(walletProvider);
+    const signer = await readWriteProvider.getSigner();
+
+    const contract = getProposalsContract(signer);
+
+    try {
+      const tx = await contract.delegate(address);
+      const receipt = await tx.wait();
+
+      if (receipt.status) {
+        return console.log("delegate successfull!");
+      }
+      console.log("delegate failed!");
+    } catch (error) {
+      console.error("error: ", error);
+    }
+  }, [address, chainId, walletProvider]);
+};
+
+export default useDelegateVote;
